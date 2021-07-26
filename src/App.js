deb346c2ac2a4049efe2142400cefb85a5ab0916
Ömer Iliski <omeriliski06@gmail.com>
2021-07-27 01:16:05 +0200

    started in linux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
+import React, {createContext, useState} from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import TabPages from './pages/TabPages';
+import LoginPage from './pages/LoginPage';
 
-import React,{createContext,useState} from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import TabPages from "./pages/TabPages"; 
-import LoginPage from "./pages/LoginPage";
-
-const Stack = createStackNavigator(); 
+const Stack = createStackNavigator();
 export const Context = createContext();
 
 function App() {
   const [registerModalVisible, setRegisterModalVisible] = useState(false);
-  const [activeModal, setActiveModal] = useState("register");
-  
+  const [activeModal, setActiveModal] = useState('register');
+
   return (
-    <Context.Provider value={{registerModalVisible,setRegisterModalVisible, activeModal, setActiveModal}}>
+    <Context.Provider
+      value={{
+        registerModalVisible,
+        setRegisterModalVisible,
+        activeModal,
+        setActiveModal,
+      }}>
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
-            headerShown: false
-          }}
-          >
-          <Stack.Screen name="LoginPage" component={LoginPage}/>
-          <Stack.Screen name="TabPages" component={TabPages}/>
+            headerShown: false,
+          }}>
+          <Stack.Screen name="LoginPage" component={LoginPage} />
+          <Stack.Screen name="TabPages" component={TabPages} />
         </Stack.Navigator>
       </NavigationContainer>
     </Context.Provider>
